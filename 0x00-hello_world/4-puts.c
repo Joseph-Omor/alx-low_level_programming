5543a0e704d538cfacabf1e0ed932e834a2c1b4c
Joseph Omor <joe.omo@hotmail.com>
2022-10-14 01:16:03 +0000

    correct 4 and added 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/* Prints Programming is like building multilingual puzzle,
+/* 
+ * Prints Programming is like building multilingual puzzle,
  * followed by a new line
  */
-int main( void )
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
