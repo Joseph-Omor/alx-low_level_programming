ab8080fb654040b4222c05bbbb6f7cccee6e2243
Joseph Omor <joe.omo@hotmail.com>
2022-10-17 23:01:57 +0000

    edited script for task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,7 +5,7 @@
  * description: print out all number of base 10, starting from 0.
  * Return 0 - program successfully executed
  */
-int main(void)
+int main(void) /*this is the main fucntion */
 {
 	int i;
 
