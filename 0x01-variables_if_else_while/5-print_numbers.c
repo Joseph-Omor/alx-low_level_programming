8d30356927bb3383786b2ec714937a5999b747b6
Joseph Omor <joe.omo@hotmail.com>
2022-10-17 22:59:26 +0000

    added solution to task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,6 +3,17 @@
 /**
  * main - main block
  * description: print out all number of base 10, starting from 0.
- * Return 0
+ * Return 0 - program successfully executed
  */
 int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
