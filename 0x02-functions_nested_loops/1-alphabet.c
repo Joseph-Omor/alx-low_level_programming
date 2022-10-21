543ed3321fde8f7538cd9ecb33b39768d3dda9de
Joseph Omoruwou <joe.omo@hotmail.com>
2022-10-21 07:34:59 -0700

    added solution to task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)/* 97 or 'a', 122 or 'z' */ 
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)/* 97 or 'a', 122 or 'z' */
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
