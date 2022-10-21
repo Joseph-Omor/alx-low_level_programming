619e899a1cb0a2c97fe0ef6b70a7a24ad4a37567
Joseph Omoruwou <joe.omo@hotmail.com>
2022-10-21 06:52:32 -0700

    added sctipt for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print putchar followed by a new line.
  *
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	putchair('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
