061539ec2e3ef810ab38dcb8794ebf151b497187
Joseph Omoruwou <joe.omo@hotmail.com>
2022-10-22 08:19:56 -0700

    added solution to task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
-
-
-
-
+/* main - this program prints alphabets a to z using ASCII representations and _putcar(a function that prints single characters)
+ *
+ */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)/* 97 or 'a', 122 or 'z' */ 
+	for (i = 97; i <= 122; i++)/* 97 or 'a' are the ASCII representation of lowercase a
+				     *
+				     * 122 or 'z' are the ASCII representation of lowercase z
+				     * 
+				     */ 
 	{
 		_putchar(i);
 	}
