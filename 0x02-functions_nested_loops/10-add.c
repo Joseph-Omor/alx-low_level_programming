9ef3ff7fe613fe61ad78ae6f27cf29a028f14782
Joseph Omoruwou <joe.omo@hotmail.com>
2022-10-22 23:43:26 -0700

    editted to suit recommended doc style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
+ * y: an integer
+ * z: an integer
+ * return: returns the addition of given parameter
  */
 int add(int y, int z)
 {
