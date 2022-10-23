fc0c8ddb2471792369bd4738424b66af791f694c
Joseph Omoruwou <joe.omo@hotmail.com>
2022-10-22 23:44:50 -0700

    edited script

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
- * y: an integer
- * z: an integer
+ * @y: an integer
+ * @z: an integer
  * return: returns the addition of given parameter
  */
 int add(int y, int z)
