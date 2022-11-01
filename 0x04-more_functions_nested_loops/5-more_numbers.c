ee39dfac9ee1d3730501bd6e02f369d5c0718ea0
Joseph Omor <joe.omo@hotmail.com>
2022-11-01 00:34:10 +0000

    removing untracked workinng tree

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers -  prints 10 times the numbers
- * from 0 to 14
- * Return: no return
- */
+* more_numbers - print 0 to 14 10 times
+*
+* Return: void
+*/
 
 void more_numbers(void)
 {
+	char c, i;
 
-int i, j;
-	for (i = 1; i <= 10; i++)
+	for(c = 0; c <= 10; c++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
+			if (c >= 10)
+				_putchar('1');
+			_putchar (c % 10 + '0');
 		}
-			_putchar('\n');
-		}
+		_putchar('\n');
+	}
 }
