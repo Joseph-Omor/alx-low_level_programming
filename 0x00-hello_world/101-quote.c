86dcb3263b07b2bc490a5b251d2dd63930ee15f4
Joseph Omor <joe.omo@hotmail.com>
2022-11-09 09:54:39 +0000

    added solution to 0x00 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
 /*
- * main - prints "and that piece of art is useful" Dora Korpar 2015-10-19",
- * followed by a new line, to standard erro. * * 
- * Return: Always 1
+ * main - prints string followed by a new line to standard error
+ *
+ * Return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\n" - Dora_Korpar, 2015-10-19\n",
-			59);
+	fprintf(stderr,"and that piece of art is useful\" - Dora_Korpar, 2015-10-19\n");
 	return (1);
 }
