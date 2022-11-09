2843c1755c2c4aad7591f8780f40866b512379d1
Joseph Omor <joe.omo@hotmail.com>
2022-11-09 10:08:13 +0000

    added solution to 0x00 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - prints string followed by a new line to standard error
+
+/**
+ * main() - prints string followed by a new line to standard error
  *
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora_Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora_Korpar, 2015-10-19\n");
 	return (1);
 }
