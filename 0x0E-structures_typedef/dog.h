cf20ecc671d083c8d8841b88e29dc12739d48e6d
Joseph Omor <joe.omo@hotmail.com>
2022-11-23 13:39:49 +0000

    added solution for project 0X0E T-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#ifndef DOG_H_
+
+/* struct dog - a struct named dog 
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H_ */
