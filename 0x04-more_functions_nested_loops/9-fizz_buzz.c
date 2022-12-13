4ef970f36ea0745e1370804eefe36cc10d908b3d
Joseph Omor <joe.omo@hotmail.com>
2022-12-13 14:04:35 +0000

    solved 0x04-9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - The Fizz Buzz test
+ *
+ */
+
+
+#define T 3
+#define F 5
+#define FF 15
+
+
+int main(void)
+{
+	int i;
+	int n = 100;
+
+
+	for (i = 1; i <= n; ++i)
+	{
+		if (i % FF == 0 && i % F == 0 && i % T == 0)
+			printf("FizzBuzz ");
+		else if (i % T == 0)
+			printf("Fizz ");
+		else if (i % F == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
