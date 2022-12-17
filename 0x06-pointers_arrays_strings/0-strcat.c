84c370af6612911201edc72e739e2309555df17d
Joseph Omor <joe.omo@hotmail.com>
2022-12-17 17:01:33 +0000

    added solution for 0x06c-0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: String one
+ * @src: String two
+ *
+ * Return: pointer to new string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	dest[i] = '\0';
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
