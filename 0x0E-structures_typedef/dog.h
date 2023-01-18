79df30a01a02d7334ea2832b14b8131f84fcc8a9
Joseph Omor <joe.omo@hotmail.com>
2023-01-18 06:56:10 +0000

    edited header dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+
+/**
+ * struct dog - structure dog
+ * @name: struct dog element #1
+ * @age: struct dog element #2
+ * @owner: struct dog element #3
+ *
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 
