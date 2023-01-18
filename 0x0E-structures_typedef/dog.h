b7121cbdab153985541d69d2d319cf1f9da70517
Joseph Omor <joe.omo@hotmail.com>
2023-01-18 08:47:28 +0000

    added solution for 0x0E.c #3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+#include <stdio.h>
 
 
 /**
@@ -9,12 +10,16 @@
  * @owner: struct dog element #3
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 
 #endif /* _DOG_H_ */
