1fb8ada0c1e3cb5adc4d1cfa38e881864b32ee06
Joseph Omor <joe.omo@hotmail.com>
2023-01-25 22:07:33 +0000

    new edits for #2's solution

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,6 +9,8 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+
+int int_index(int *array, int size, int (*cmp)(int));
 void print_elem_hex(int elem);
 void print_elem(int elem);
 
