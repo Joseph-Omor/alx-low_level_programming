0114944b397a95c8363fc48b045ae4e72b40f083
Joseph Omor <joe.omo@hotmail.com>
2023-01-25 16:16:14 +0000

    editted 3-main and 3-get_op.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*ops[i].op == *s)
+		if (*(ops[i].op) == *s)
 			return (ops[i].f);
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 	int (*operator)(int, int);
 	int num1, num3, result;
 
-	if (argc > 5 || argc < 2)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
