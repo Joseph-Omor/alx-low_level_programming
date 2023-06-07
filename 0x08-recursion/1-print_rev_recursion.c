b3e128ed594dba4a7787b2f2e52f9cb0eaa66d93
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-07 16:50:07 +0100

    fixed betty coding style error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: string to print
- * 
+ *
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		printf("%c", *s);
