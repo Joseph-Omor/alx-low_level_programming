03ee29280d55a85af8e2247a8e574d5be5d44713
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-09 00:16:12 +0100

    removed loop from solution, no loops allowed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,19 +9,14 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n < 2)
 	{
 		return (0);
 	}
 
-	for (i = 2; i * i <= n; i++)
+	if (n % 2 == 0)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (0);
 	}
 
 	return (1);
