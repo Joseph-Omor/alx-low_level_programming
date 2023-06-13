a26daccd600a9003b8372e69c2d2243acba00093
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-13 09:50:39 +0100

    A program that prints a count of its arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a count of its arguments
+ * @argc: argument count
+ * @argv: agrument vector (array of argument strings) *UNUSED
+ *
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
