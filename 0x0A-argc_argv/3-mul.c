7dff035def7b9730be9c3f4285ffa673649feef7
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-13 13:06:56 +0100

    update: used a loop to print incase more than 2 arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,37 @@
 
 int main(int argc, char *argv[])
 {
-	int mul;
+	int result;
+	int i;
+	int num;
+	char *lastptr;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", mul);
+	result = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		num = strtol(argv[i], &lastptr, 10);
+
+		if (*lastptr != '\0')
+		{
+			return (1);
+		}
+
+		result *= num;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+
+
+	printf("%d\n", result);
 
 	return (0);
 }
