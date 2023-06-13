cfe8164ae79b0e71fdc23bd80a802388764b2f45
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-13 09:44:42 +0100

    A script that prints it's name when compiled

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: argument count
+ * @argv: agrument vector (array of argument strings)
+ *
+ * Return: 0 if successful.
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
