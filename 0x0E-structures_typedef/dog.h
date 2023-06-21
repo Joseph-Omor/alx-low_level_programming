39a62394d358c63f42b70612bb2ff52ee28fe64b
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-21 19:05:14 +0100

    Added header: struct dog defined with include guards and struct doc

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a user-defined structure
+ * @name: ditto
+ * @age: ditto
+ * @owner: ditto
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
