d1d9d1295fcd9ab741fc72fc1b88b0ff1235f47a
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-22 22:27:12 +0100

    added function that executes a function for array elements

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 
-void print_name(char *name, void (*f)(char *));
+#include <stdlib.h>
 
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif /* _FUNCTION_POINTERS_H */
