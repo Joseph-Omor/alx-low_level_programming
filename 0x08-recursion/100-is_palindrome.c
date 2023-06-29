c96be14b4a6ee09ed82c5ab3ff58968d45b8c74c
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-06-29 16:36:21 +0100

    update code to function recursively

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,46 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * is_palindrome - checkes if string is a palindrome
  * @s: string to check
  *
- * Return: 0 if not and 1 if palindrom
+ * Return: 0 if not and 1 if palindrome
  */
-
 int is_palindrome(char *s)
 {
-	int i, j;
-	int len;
+	int length, result;
+	char *substring = NULL;
 
-	len = strlen(s);
+	if (s == NULL)
+	{
+		return (0);
+	}
 
-	if (len == 0)
+	length = strlen(s);
+
+	/* Base case: if the length is 0 or 1, it's a palindrome */
+	if (length <= 1)
 	{
 		return (1);
 	}
-
-	for (i = 0, j = len - 1; i < j; i++, j--)
+	/* Compare the first and last characters */
+	if (s[0] != s[length - 1])
 	{
-		if (s[i] != s[j])
-		{
-			return (0);
-		}
+		return (0);
 	}
 
-	return (1);
+	/* Create a copy of the string without the first and last characters */
+	substring = malloc(sizeof(length) - 1);
+	strncpy(substring, s + 1, length - 2);
+	substring[length - 2] = '\0';
+
+
+	/* Recursively check the substring */
+	result = is_palindrome(substring);
+
+	free(substring);
+
+	return (result);
 }
