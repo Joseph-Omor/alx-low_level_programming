7c98d0c91afa74120d2d01a25fa19316c058fb89
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-07-04 23:09:48 +0100

    refactored code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,27 +10,36 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		exit(1);
+		return;
 	}
 
+	/* Print the name of the dog */
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	else
-		printf("Name: %s\n", d->name);
-
-	if (d->age == -1.0)
 	{
-		printf("Age: (nil)");
+		printf("Name: %s\n", d->name);
+	}
+
+	/* Print the age of the dog */
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
 	}
 	else
+	{
 		printf("Age: %f\n", d->age);
+	}
 
+	/* Print the owner of the dog */
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
+	{
 		printf("Owner: %s\n", d->owner);
+	}
 }
