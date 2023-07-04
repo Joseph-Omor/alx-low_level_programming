086303661efb94acdb1a8f3b869047721c2c4ab5
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-07-04 23:13:03 +0100

    solved task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: ditto
  * @owner: ditto
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
