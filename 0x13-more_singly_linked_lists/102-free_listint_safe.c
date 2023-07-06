36b4a31285d0f8509cc3f9b8cc22da54102d9833
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-07-06 14:29:18 +0100

    added a function that safely free's a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint_safe - A function that frees a linked list in a safe and
+ * efficient manner.
+ * @h: head pointer to the linked list
+ *
+ * Return: size variable representing the number of nodes freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current = *h;
+	listint_t *next;
+	size_t size = 0;
+
+	/* iterate through the list and perform operation */
+	while (current != NULL)
+	{
+		/* store the next node in the 'next' pointer */
+		next = current->next;
+		/* deallocate the memory occupied by the current node */
+		free(current);
+		/* increment variable to keep track of the number of nodes */
+		size++;
+		/* move current pointer to next node to continue traversal */
+		current = next;
+	}
+	*h = NULL;
+	return (size);
+}
