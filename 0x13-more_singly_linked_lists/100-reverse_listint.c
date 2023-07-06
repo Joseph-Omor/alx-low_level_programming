00afcc2eb58e50e84af0589119a189af89d18fe9
Joseph Omoruwou <josephomorwuou@gmail.com>
2023-07-06 09:41:14 +0100

    update: removed unneccessary NULL assignments

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,18 +9,23 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next_node = NULL;
+	listint_t *prev;
+	listint_t *next_node;
 	listint_t *current = *head;
 
 	while (current != NULL)
 	{
+		/* Store the next node */
 		next_node = current->next;
+		/* Reverse the pointer of the current node */
 		current->next = prev;
+		/* Move the prev pointer to the current node */
 		prev = current;
+		/* Move the current pointer to the next node */
 		current = next_node;
 	}
 
+	/* Update the head pointer to the new first node */
 	*head = prev;
 	return (*head);
 }
