39630df919295ee0d89b6be87c456a37b313800d
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-10 04:38:17 +0100

    added program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
