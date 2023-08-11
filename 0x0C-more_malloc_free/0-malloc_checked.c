1188a0771b96d61dd0d781f57677f6f58c32c5be
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-11 03:18:36 +0100

    added a function that allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * malloc_checked - A function that allocates memory using malloc
+ * @b: parameter to use as size
+ *
+ * Return: Pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
