af417e89bb2dade62eb7c84539705ead0d0da73c
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-14 05:49:20 +0100

    added a function #2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * _calloc - A function that allocates memory for an array
+ * @nmemb: number of array elements to allocate
+ * @size: size of each element
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t total_size;
+	void *allocated_memory;
+
+	total_size = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	allocated_memory = malloc(total_size);
+
+	if (allocated_memory == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(allocated_memory, 0, total_size);
+
+	return (allocated_memory);
+}
