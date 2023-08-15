1c8a0fa42e84a583378d429eeefee459336d10a5
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-16 00:09:15 +0100

    added a check for NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - a fucntion that initializes a struct dog
@@ -9,6 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
