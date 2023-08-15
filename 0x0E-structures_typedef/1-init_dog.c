41d06fa3d7746e9e0edafdf13e3ee8552cc3d832
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-15 23:48:58 +0100

    updated code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,19 +2,14 @@
 
 /**
  * init_dog - a fucntion that initializes a struct dog
- * @d: pointer to struct
+ * @d: pointer to the struct
  * @name: name member
  * @age: age member
  * @owner: owner member
- *
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *dptr;
-
-	dptr = d;
-
-	(*dptr).name = name;
-	(*dptr).age = age;
-	(*dptr).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
