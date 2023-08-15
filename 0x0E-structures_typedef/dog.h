f07ceed88c6d09b6b04263e9e75ef1a53ad9398b
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-15 21:19:03 +0100

    added head for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct dog
+ * @name: struct member name
+ * @age: struct member age
+ * @owner: struct member owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG_H */
