1beb817f4b0d7103a6e5df6c8bd1abb394caa5ab
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-08-16 11:48:57 +0100

    added a new function #4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - A function that creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = (dog_t *)malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
