5fba2286eed2117a28bf68052166ef6e93ff39fc
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-07-18 06:01:33 +0100

    update file: fixed for betty requirement v0.2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,8 +7,7 @@
  * @array: pointer to the first element of the array
  * @size: number of elements in array
  * @value: value to search for
- * Return: index where value is located
- * or -1 if array is NULL or value is not found
+ * Return: index where value is located or -1 if value is not found
  */
 int linear_search(int *array, size_t size, int value)
 {
